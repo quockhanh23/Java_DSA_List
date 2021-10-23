a8364ac27f7e810d99666e19eff60426672c6de0
unknown <yeuthuongvoivangdiqua@gmail.com>
2021-10-23 20:49:25 +0700

    1

---
+++ src/TH2/Test.java
@@ -0,0 +1,15 @@
+package TH2;
+
+public class Test {
+    public static void main(String[] args) {
+
+        MyLinkedList linkedList = new MyLinkedList(10);
+        linkedList.addFirst(11);
+        linkedList.addFirst(12);
+        linkedList.addFirst(13);
+
+        linkedList.add(4,9);
+        linkedList.add(4,9);
+        linkedList.printList();
+    }
+}
